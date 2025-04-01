602e75046594a38f1a5778259362c38c17480f05
Lavan08-op <lavanyasaini615@gmail.com>
2025-04-02 02:20:52 +0530

    Add files via upload

---
+++ portfolio web/script.js
@@ -0,0 +1,8 @@
+document.querySelector(".social").addEventListener("click", function () {
+  window.location.href =
+    "https://www.linkedin.com/in/lavanya-saini-556a36324?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+});
+document.querySelector(".insta").addEventListener("click", function () {
+  window.location.href =
+    "https://www.instagram.com/lightning_spark08?igsh=MXdodTVtYjQ5cjdpcA==";
+});
